1b64c23b0e0e185e7bbc5a42ce3885eff1b15803
JYB <7303639+BingoJYB@users.noreply.github.com>
2019-11-21 00:32:41 +0100

    add avatar

--- app/routes.py
+++ app/routes.py
@@ -14,7 +14,8 @@ def index():
     user = {"username": "Miguel"}
     posts = [
         {"author": {"username": "John"}, "body": "flask day in Portland!"},
-        {"author": {"username": "Susan"}, "body": "The Avengers movie was so cool!"},
+        {"author": {"username": "Susan"}, "body": "The Avengers\
+             movie was so cool!"},
     ]
     return render_template("index.html", title="Home", posts=posts)
 

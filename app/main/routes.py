b27d36ebaeb662ed2223cd2ada888254928efa0e
JYB <7303639+BingoJYB@users.noreply.github.com>
2020-12-03 23:46:06 +0100

    add full text search

--- app/main/routes.py
+++ app/main/routes.py
@@ -5,12 +5,10 @@ from flask_login import current_user, login_required
 from flask_babel import _, get_locale
 from guess_language import guess_language
 from app import db
-from app.main.forms import EditProfileForm, EmptyForm, PostForm
+from app.main.forms import EditProfileForm, EmptyForm, PostForm, SearchForm
 from app.models import User, Post
 from app.translate import translate
 from app.main import bp
-from flask import g
-from app.main.forms import SearchForm
 
 
 @bp.before_app_request
